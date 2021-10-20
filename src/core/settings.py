a4df388d51f28f0855bdade6ccd9625696b86c7b
yauheni <Yauheni.conn@gmail.com>
2021-10-20 16:19:11 +0300

    feature - add Angular

--- src/core/settings.py
+++ src/core/settings.py
@@ -128,7 +128,7 @@ THUMBNAIL_BASEDIR = "thumbnails"
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "ru"
+LANGUAGE_CODE = "en"
 
 TIME_ZONE = "UTC"
 
