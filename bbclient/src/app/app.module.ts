90096602c280e673313d24bec8e6bc5119133dea
yauheni <Yauheni.conn@gmail.com>
2021-10-20 19:13:10 +0300

    feature - add Angular

--- bbclient/src/app/app.module.ts
+++ bbclient/src/app/app.module.ts
@@ -4,6 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BbListComponent } from './bb-list.component';
 import { BbDetailComponent } from './bb-detail.component';
+import { BbService } from "./bb.service";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { registerLocaleData } from "@angular/common";
+import localeRu from "@angular/common/locales/ru";
+import localeRuExtra from "@angular/common/locales/extra/ru";
+import { LOCALE_ID } from "@angular/core";
+import { Routes } from "@angular/router";
+import { RouterModule } from "@angular/router";
+
+registerLocaleData(localeRu, "ru", localeRuExtra);
+
+const appRoutes: Routes =[
+  {path: ":pk", component: BbDetailComponent},
+  {path: "", component: BbListComponent},
+];
 
 @NgModule({
   declarations: [
@@ -12,9 +28,15 @@ import { BbDetailComponent } from './bb-detail.component';
     BbDetailComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    BbService,
+    {provide: LOCALE_ID, useValue: "ru"}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- bbclient/src/app/bb.service.ts
+++ bbclient/src/app/bb.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { of } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BbService {
+  private url: String = "http://localhost:8000";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getBbs(): Observable<Object[]> {
+    return this.http.get<Object[]>(this.url + "/api/bbs/");
+  }
+
+  getBb(pk: Number): Observable<Object> {
+    return this.http.get<Object>(this.url + "/api/bbs/" + pk);
+  }
+
+  getComment(pk: Number): Observable<Object[]> {
+    return this.http.get<Object[]>(this.url + "/api/bbs/" + pk + "/comments/");
+  }
+
+  addComment(bb: String, author: String, password: String, content: String): Observable<Object> {
+    const comment = {"bb": bb, "author": author, "content": content};
+    const options = {headers: new HttpHeaders(
+        {"Content-Type": "application/json",
+        "Authorization": "Basic " + window.btoa(author + ":" + password
+        )})};
+    return this.http.post<Object>(this.url + "/api/bbs/" + bb + "/comments/", comment, options).pipe(catchError(this.handleError()));
+  }
+
+  handleError() {
+    return (error: any): Observable<Object> => {
+      window.alert(error.message);
+      return of();
+    }
+  }
 }
